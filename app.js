40720743cb3082035173632a6baafd5c7ffa08cc
rarice4 <rarice@g.cofc.edu>
2016-10-16 19:10:29 -0400

    add promise library and setup get Accounts with Authorized client

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var Gdax = require('gdax');
 var access = require('./access.js');
+var Promise = require('promise')
 console.log("Access:", access);
 var publicClient = new Gdax.PublicClient();
 var authedClient = new Gdax.AuthenticatedClient(access.key, access.b64secret, access.passphrase);
 var callback = function(err, response, data) {
   console.log(response.body);
 };
-publicClient.getProducts(callback);
+authedClient.getAccounts(function(err, response, data) {
+  console.log(JSON.parse(response.body)[0]);
+  return JSON.parse(response.body)[0];
+});
